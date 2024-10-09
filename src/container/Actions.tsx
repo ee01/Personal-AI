842537075ce1635b99454d647bcbaabeb1615a31
Spike Yang <spike.yang@ringcentral.com>
2024-10-09 16:01:12 +0800

    update action button

--- src/container/Actions.tsx
+++ src/container/Actions.tsx
@@ -24,20 +24,20 @@ export const Actions = (props: IActionsProps) => {
         handleDelete,
         handleGenerateReport,
         handleGenerateDisposeReport,
-        handleGenerateGlobalSearchReport,
+        // handleGenerateGlobalSearchReport,
         handleTrendingTopics,
-        handleGenerateSalesReport,
+        // handleGenerateSalesReport,
     } = props;
 
     return (
         <div className="radar-poc-config-section">
             {showConfig && <button className="radar-poc-result-button" onClick={handleSaveConfig}>Save Configuration</button>}
             <button className="radar-poc-result-button" onClick={handleGenerateReport} disabled={!latestTimestamp}>GraphRAG - Generate Report</button>
-            <button className="radar-poc-result-button" onClick={handleGenerateSalesReport} disabled={!latestTimestamp}>GraphRAG - Generate Sales Report</button>
-            <button className="radar-poc-result-button" onClick={handleInitialize}>GraphRAG Initialize</button>
+            {/* <button className="radar-poc-result-button" onClick={handleGenerateSalesReport} disabled={!latestTimestamp}>GraphRAG - Generate Sales Report</button> */}
+            <button className="radar-poc-result-button" onClick={handleInitialize}>GraphRAG Indexing</button>
             <button className="radar-poc-result-button" onClick={handleIncrement} disabled={!latestTimestamp}>GraphRAG Increment</button>
             <button className="radar-poc-result-button" onClick={handleDelete} disabled={!latestTimestamp}>GraphRAG Delete All Indexing</button>
-            <button className="radar-poc-result-button" onClick={handleGenerateGlobalSearchReport} disabled={!latestTimestamp}>GraphRAG - Generate Report(Use Global Search)</button>
+            {/* <button className="radar-poc-result-button" onClick={handleGenerateGlobalSearchReport} disabled={!latestTimestamp}>GraphRAG - Generate Report(Use Global Search)</button> */}
             <button className="radar-poc-result-button" onClick={handleTrendingTopics} disabled={!latestTimestamp}>GraphRAG - Trending Topics</button>
             <button className="radar-poc-result-button" onClick={handleGenerateDisposeReport}>[Full Context for Dify] - Generate Report</button>
         </div>
