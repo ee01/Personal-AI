8e8fcaf457251678e18964f806594d1cf3f33b83
Esone.Q <esone.qiu@ringcentral.com>
2025-05-08 22:40:01 +0800

    support env.development to manifest.json

--- webpack.dev.cjs
+++ webpack.dev.cjs
@@ -1,10 +1,25 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.cjs');
+const Dotenv = require('dotenv-webpack');
+const path = require('path');
 
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-});
+module.exports = (env) => {
+  // 加载开发环境变量
+  const dotenv = require('dotenv').config({ path: '.env.development' });
+  
+  return merge(common({
+    ...env,
+    GOOGLE_CLIENT_ID: dotenv.parsed.GOOGLE_CLIENT_ID
+  }), {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+      static: './dist',
+    },
+    plugins: [
+      new Dotenv({
+        path: '.env.development'
+      })
+    ]
+  });
+};
